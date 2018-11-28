15b594abae50cd3cf6f16bf6fa27880faf2f2063
bdguodong <bdguodong@gmail.com>
2018-11-28 17:44:20 +0800

    change notification from link to text

--- cabot_alert_dingtalk/models.py
+++ cabot_alert_dingtalk/models.py
@@ -83,24 +83,21 @@ class DingtalkAlert(AlertPlugin):
             })
 
         # TODO: handle color
-
-        # link
-        # {
-        #     "msgtype": "link",
-        #     "link": {
-        #         "text":"群机器人是钉钉群的高级扩展功能。群机器人可以将第三方服务的信息聚合到群聊中，实现自动化的信息同步。例如：通过聚合GitHub，GitLab等源码管理服务，实现源码更新同步；通过聚合Trello，JIRA等项目协调服务，实现项目信息同步。不仅如此，群机器人支持Webhook协议的自定义接入，支持更多可能性，例如：你可将运维报警提醒通过自定义机器人聚合到钉钉群。",
-        #         "title": "自定义机器人协议",
-        #         "picUrl": "",
-        #         "messageUrl": "https://open-doc.dingtalk.com/docs/doc.htm?spm=a219a.7629140.0.0.Rqyvqo&treeId=257&articleId=105735&docType=1"
+        # text
+        #     "msgtype": "text",
+        #     "text": {
+        #         "content": "我就是我,  @1825718XXXX 是不一样的烟火"
+        #     },
+        #     "at": {
+        #         "atMobiles": [
+        #             "1825718XXXX"
+        #         ],
+        #         "isAtAll": false
         #     }
-        # }
         ding_notification = {
-            'msgtype': 'link',
-            'link': {
-                'text': message,
-                'title': service.name,
-                'picUrl': '',
-                'messageUrl': ''
+            'msgtype': 'text',
+            'text': {
+                'content': message,
             }
         }
         headers = {'Content-Type': 'application/json; charset=utf-8'}
